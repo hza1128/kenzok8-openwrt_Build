5f089a76110ba35713c92cc2a56feec9b512e8d4
hza1128 <141570215+hza1128@users.noreply.github.com>
2025-03-27 21:26:33 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -13,3 +13,5 @@ sed -i 's/192.168.1.1/192.168.10.11/g' openwrt/package/base-files/files/bin/conf
 #sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
 #sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
 #sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
+# 设置默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' feeds/luci/collections/luci/Makefile
